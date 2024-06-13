9545733701e74518c8d2e23eb52a547bab560c07
flysand7 <thebumboni@gmail.com>
2024-06-13 21:22:37 +1100

    Remove extra stuff

--- util/damage.py
+++ util/damage.py
@@ -3,23 +3,6 @@ import sympy as sp
 from .character import Character
 from .weapon import Weapon
 
-# def define_damage_formula():
-#     CharacterATK, WeaponATK, bonusATK_percent, bonus_flat_ATK = sp.symbols('CharacterATK WeaponATK bonusATK_percent bonus_flat_ATK')
-#     SkillMultiplier, SkillScalingBonus = sp.symbols('SkillMultiplier SkillScalingBonus')
-#     bonusElementalDamage, bonusSkillDamage = sp.symbols('bonusElementalDamage bonusSkillDamage')
-#     deepen, CriticalDamageMultiplier = sp.symbols('TotalDeepenEffect CriticalDamageMultiplier')
-#     res_enemy, res_reduction = sp.symbols('EnemyResistance ResistanceReduction')
-#     damage_formula = (
-#         ((CharacterATK + WeaponATK) * (1 + bonusATK_percent) + bonus_flat_ATK)
-#         * (SkillMultiplier * (1 + SkillScalingBonus))
-#         * (1 + bonusElementalDamage + bonusSkillDamage)
-#         * (1 + deepen)
-#         * CriticalDamageMultiplier
-#         * 0.48 * (1 - res_enemy + res_reduction)
-#     )
-#     return damage_formula
-# damage_formula = define_damage_formula()
-
 def simple_damage(character: Character, weapon: Weapon, move_id: str) -> tuple[float, float]:
     # Calculate the character stats
     base_atk = character.base_atk() + weapon.base_atk()
@@ -35,7 +18,7 @@ def simple_damage(character: Character, weapon: Weapon, move_id: str) -> tuple[f
     # test case. I need to verify everything, potentially re-derive the formula
     # for damage for which I'll need lots of testing.
     enemy_base_res = 0.1
-    res_penetration = 0.0
+    res_penetration = 0.3
     res_total = enemy_base_res + res_penetration
     em_res = \
         (1 - res_total/2) if res_total < 0 else \
